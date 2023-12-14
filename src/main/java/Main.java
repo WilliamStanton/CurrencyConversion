c061ca2ce6497d8765638c940fc57d7de5acd740
billy <billy@10.40.64.104>
2023-12-14 09:49:56 -0500

    New changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,30 +8,102 @@
  */
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 public class Main {
     
     public static Currencies currencyAPI = new Currencies();
-    public static ImageIcon icon = new ImageIcon("logo.png");
+    public static ImageIcon icon = new ImageIcon("icon.png");
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf");
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        
+        // Initialize Currencies
+        String[] currencies = {
+            "USD",
+            "EUR",
+            "JPY",
+            "GBP",
+            "AUD",
+            "CAD",
+            "CNY",
+            "CNH",
+            "HKD",
+            "NZD"
+        };
+        
         // Option Dialog Elements
-        JLabel title = new JLabel("Currency Conversion Application");
-        title.setFont(new Font("Arial", Font.BOLD, 14));
+        // App Title
+        JLabel appTitle = new JLabel("Currency Conversion Application");
+        appTitle.setFont(new Font("Arial", Font.BOLD, 24));
          
+        // Base Currency
+        // Base Currency Title
+        JLabel baseCurrencyTitle = new JLabel("Original currency");
+        baseCurrencyTitle.setFont(new Font("Arial", Font.PLAIN, 18));
         
+        // Base Currency Textbox
+        JTextField baseCurrencyTextbox = new JTextField();
         
+        // Base Currency Dropdown
+        JComboBox baseCurrencyDropdown = new JComboBox(currencies);
         
+        // Target Currency
+        // Target Currency Title
+        JLabel targetCurrencyTitle = new JLabel("Target currency");
+        targetCurrencyTitle.setFont(new Font("Arial", Font.PLAIN, 18));
+        
+        // Target Currency Dropdown
+        JComboBox targetCurrencyDropdown = new JComboBox(currencies);
+
+        // Convert the currency
+        JButton convert = new JButton("Convert Currencies");
+        ActionListener goConvert = (ActionEvent ae) -> {
+            // Get results
+            double results = currencyAPI.getConversion(Double.parseDouble(
+                  baseCurrencyTextbox.getText()),                           // base currency
+                    currencies[baseCurrencyDropdown.getSelectedIndex()],      // base currency amount
+                    currencies[targetCurrencyDropdown.getSelectedIndex()]);   // target currency
+            
+            // Display results
+            JOptionPane.showMessageDialog(convert, new DecimalFormat("#.00").format(Double.parseDouble(baseCurrencyTextbox.getText())) + " " + currencies[baseCurrencyDropdown.getSelectedIndex()] + " is " + new DecimalFormat("#.00").format(results) + " " + currencies[targetCurrencyDropdown.getSelectedIndex()], "Currency Conversion Application", 0, icon);
+        };
+        convert.addActionListener(goConvert);
         
         // Display Array
         Object[] display = {
+            // App Title
+            appTitle,
             
+            // Base Currency
+            baseCurrencyTitle,
+            baseCurrencyTextbox, baseCurrencyDropdown,
+            
+            // Target Currency
+            targetCurrencyTitle,
+            targetCurrencyDropdown,
+            
+            // Convert the currency
+            convert
         };
         
         // Display UI
         JOptionPane.showOptionDialog(null, display, "Currency Conversion Application", 0, -1, icon, new Object[]{}, null);
+        
+        // Safely exit
+        System.exit(0);
     }
 }
