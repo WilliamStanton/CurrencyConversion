ec4c8a8af872b2bbce60fc9e9abfcb6267cc7ae8
moofie <moofie@moofie.xyz>
2023-12-15 19:24:53 -0500

    Changes

--- src/main/java/Currencies.java
+++ src/main/java/Currencies.java
@@ -1,14 +1,15 @@
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import javax.swing.JOptionPane;
 
 /**
  * The Currencies class provides various methods for conversion of currencies
@@ -19,35 +20,111 @@ import java.net.URL;
  */
 public class Currencies {
     
-    public double getConversion(double baseAmount, String baseCurrency, String targetCurrency) throws MalformedURLException, IOException {
-        // GET request
-        URL url = new URL("https://v6.exchangerate-api.com/v6/306aa222cfd43e8ff567e5f3/pair/" + baseCurrency + "/" + targetCurrency + "/" + baseAmount);
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
-        System.out.println(request.toString());
+    // Init Variables
+    private String apiKey;
+    private String[][] supportedCurrencies;
+    
+    // No-arg Constructor
+    public Currencies() {
+        // Attempt to load API Key from ENV
+        try {
+            // Load env
+            Dotenv dotenv = Dotenv.load();
+            apiKey = dotenv.get("API_KEY"); // grab API Key
+            
+            // Try api key
+            try {
+                URL url = new URL("https://v6.exchangerate-api.com/v6/" + apiKey + "/codes");
+                HttpURLConnection request = (HttpURLConnection) url.openConnection();
+                request.connect();
+                // If api key is invalid, exit
+                if (request.getResponseCode() != 200) {
+                    JOptionPane.showMessageDialog(null, "INVALID API KEY", "Currency Conversion Application", 0, null);
+                    System.exit(0);
+                }
+            }
+            catch(MalformedURLException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Currency Conversion Application", 0, null);
+                System.exit(0);
+            }
+            catch(IOException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Currency Conversion Application", 0, null);
+                System.exit(0);
+            }
+        }
+        catch(ExceptionInInitializerError e) {
+                JOptionPane.showMessageDialog(null, "UNABLE TO LOAD API KEY", "Currency Conversion Application", 0, null);
+                System.exit(0);
+        }
         
-        // Parse JSON Response
-        JsonParser jp = new JsonParser();
-        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-        JsonObject jsonobj = root.getAsJsonObject();
-        
-        System.out.println(jsonobj.getAsJsonObject());
-        
-        return jsonobj.get("conversion_result").getAsDouble();
+        // Load supported currencies
+        try {
+            // GET request
+            URL url = new URL("https://v6.exchangerate-api.com/v6/" + apiKey + "/codes");
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            
+            // Parse JSON Response
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonobj = root.getAsJsonObject();
+
+            // Get nested JSON
+            JsonArray results = jsonobj.getAsJsonArray("supported_codes");
+            supportedCurrencies = new String[results.size()][2];
+            
+            // Dump to array
+            for (int i = 0; i < results.size(); i++) {
+                String currencyResults = results.get(i).toString().replaceAll("\\[|\\]|\"", "");
+                int splitIndex = currencyResults.indexOf(",");
+                supportedCurrencies[i][0] = currencyResults.substring(0, splitIndex); // currency code
+                supportedCurrencies[i][1] = currencyResults.substring(splitIndex+1); // currency name
+            }
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
     
-//    public void getRate(String currency) throws MalformedURLException, IOException {
-//        // GET request
-//        URL url = new URL("https://v6.exchangerate-api.com/v6/306aa222cfd43e8ff567e5f3/latest/" + currency);
-//        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-//        request.connect();
-//        System.out.println(request.toString());
-//
-//        // Parse JSON Response
-//        JsonParser jp = new JsonParser();
-//        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-//        JsonObject jsonobj = root.getAsJsonObject();
-//
-//        System.out.println(jsonobj.get("conversion_rates").getAsJsonObject().get("EUR"));
-//    }
+    /**
+     * The getConversion method method accepts a base amount, base currency, and converts it to the amount
+     * equivalent to the accepted target currency
+     * 
+     * @param baseAmount the base amount
+     * @param baseCurrency the currency that the base amount is in
+     * @param targetCurrency the target currency to convert the base to
+     * 
+     * @return the amount in the target currency
+     */
+    public double getConversion(double baseAmount, String baseCurrency, String targetCurrency) {
+        // GET request
+        try {
+            URL url = new URL("https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/" + baseCurrency + "/" + targetCurrency + "/" + baseAmount);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            System.out.println(request.toString());
+
+            // Parse JSON Response
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonobj = root.getAsJsonObject();
+            
+            // Return the result
+            return jsonobj.get("conversion_result").getAsDouble();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return -1;
+    }
+    
+    /**
+     * The getSupportedCurrencies method returns a 2d array containing currency codes and relating currency names
+     * 
+     * @return 2d array containing the currency code and currency name
+     */
+    public String[][] getSupportedCurrencies() {
+        return supportedCurrencies;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,26 +33,21 @@ public class Main {
             System.err.println("Failed to initialize LaF");
         }
         
-//        currencyAPI.getRate("USD");
-        
-        // Initialize Currencies
-        String[] currencies = {
-            "USD",
-            "EUR",
-            "JPY",
-            "GBP",
-            "AUD",
-            "CAD",
-            "CNY",
-            "CNH",
-            "HKD",
-            "NZD"
-        };
+        // Load currencies
+        String[][] supportedCurrencies = currencyAPI.getSupportedCurrencies();
+        String[] supportedCurrenciesNames = new String[supportedCurrencies.length];
+        for (int i = 0; i < supportedCurrencies.length; i++) {
+            supportedCurrenciesNames[i] = supportedCurrencies[i][0] + ", " + supportedCurrencies[i][1];
+        }
         
         // Option Dialog Elements
         // App Title
         JLabel appTitle = new JLabel("Currency Conversion Application");
         appTitle.setFont(new Font("Arial", Font.BOLD, 24));
+        
+        // Statistics Title
+        JLabel statTitle = new JLabel(supportedCurrencies.length + " currencies");
+        statTitle.setFont(new Font("Arial", Font.BOLD, 20));
          
         // Base Currency
         // Base Currency Title
@@ -63,7 +58,7 @@ public class Main {
         JTextField baseCurrencyTextbox = new JTextField();
         
         // Base Currency Dropdown
-        JComboBox baseCurrencyDropdown = new JComboBox(currencies);
+        JComboBox baseCurrencyDropdown = new JComboBox(supportedCurrenciesNames);
         
         // Target Currency
         // Target Currency Title
@@ -71,7 +66,7 @@ public class Main {
         targetCurrencyTitle.setFont(new Font("Arial", Font.PLAIN, 18));
         
         // Target Currency Dropdown
-        JComboBox targetCurrencyDropdown = new JComboBox(currencies);
+        JComboBox targetCurrencyDropdown = new JComboBox(supportedCurrenciesNames);
 
         // Convert the currency
         JButton convert = new JButton("Convert Currencies");
@@ -79,14 +74,14 @@ public class Main {
             // Get results
             try {
                 double results = currencyAPI.getConversion(Double.parseDouble(
-                        baseCurrencyTextbox.getText()), // base currency
-                        currencies[baseCurrencyDropdown.getSelectedIndex()], // base currency amount
-                        currencies[targetCurrencyDropdown.getSelectedIndex()]);   // target currency
+                      baseCurrencyTextbox.getText()), // base currency
+                        supportedCurrenciesNames[baseCurrencyDropdown.getSelectedIndex()].replaceAll(",.*", ""), // base currency amount
+                        supportedCurrenciesNames[targetCurrencyDropdown.getSelectedIndex()].replaceAll(",.*", ""));   // target currency
 
                 // Display results
-                JOptionPane.showMessageDialog(convert, new DecimalFormat("#.00").format(Double.parseDouble(baseCurrencyTextbox.getText())) + " " + currencies[baseCurrencyDropdown.getSelectedIndex()] + " is " + new DecimalFormat("#.00").format(results) + " " + currencies[targetCurrencyDropdown.getSelectedIndex()], "Currency Conversion Application", 0, icon);
+                JOptionPane.showMessageDialog(convert, new DecimalFormat("#.00").format(Double.parseDouble(baseCurrencyTextbox.getText())) + " " + supportedCurrenciesNames[baseCurrencyDropdown.getSelectedIndex()].replaceAll(",.*", "") + " is " + new DecimalFormat("#.00").format(results) + " " + supportedCurrenciesNames[targetCurrencyDropdown.getSelectedIndex()].replaceAll(",.*", ""), "Currency Conversion Application", 0, icon);
             }
-            catch(HeadlessException | IOException | NumberFormatException e) {
+            catch(HeadlessException | NumberFormatException e) {
                 JOptionPane.showMessageDialog(convert, "Invalid Input \n" + e.toString(), "Currency Conversion Application", 0, icon);
             }
         };
@@ -96,6 +91,7 @@ public class Main {
         Object[] display = {
             // App Title
             appTitle,
+            statTitle,
             
             // Base Currency
             baseCurrencyTitle,
